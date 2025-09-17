c97d90ec4c5ac3ee76a33d3504add92a5a4ec41e
Zdenko Fabšič <zdenko@fabsic.com>
2025-09-17 19:33:32 +0200

    Update cors.go

--- siwx/cors/cors.go
+++ siwx/cors/cors.go
@@ -1,17 +1,17 @@
 package cors
 
 import (
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Register() fiber.Handler {
-    return cors.New(cors.Config{
-        AllowOriginsFunc: func(_ string) bool {
-            return true
-        },
-        AllowMethods:     "GET,HEAD,POST,PUT,DELETE,OPTIONS,PATCH",
-        AllowHeaders:     "Origin, Content-Type, Accept, Authorization, Upgrade, Connection, Sec-WebSocket-Key, Sec-WebSocket-Version, Sec-WebSocket-Extensions",
-        AllowCredentials: true,
-    })
+	return cors.New(cors.Config{
+		AllowOriginsFunc: func(_ string) bool {
+			return true
+		},
+		AllowMethods:     "GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH",
+		AllowHeaders:     "Origin, Content-Type, Accept, Cache-Control, Pragma, Referer, User-Agent, Authorization, Upgrade, Connection, Sec-Ch-Ua, Sec-Ch-Ua-Mobile, Sec-Ch-Ua-Platform, Sec-WebSocket-Key, Sec-WebSocket-Version, Sec-WebSocket-Extensions, Guardian-Api-Key, X-Guardian-Api-Key, Guardian-Webhook-Key, X-Guardian-Webhook-Key, X-Key",
+		AllowCredentials: true,
+	})
 }
